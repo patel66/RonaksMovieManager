ff9586005eca38e34d75863e5083c5952174596a
patel66 <patel66@sbcglobal.net>
2014-10-10 04:57:05 -0500

    Added validations for Movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :title, :duration, :format, :rating, :release_year, presence: true
+  validates_numericality_of :duration, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 500, :message => "Enter a realistic number."
+  validates_numericality_of :release_year, :greater_than_or_equal_to => 1900, :less_than_or_equal_to => 2020, :message => "Enter a realistic year."
 end
